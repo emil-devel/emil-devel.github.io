f128aa6716a5993b3b3a1b2c9d64aa99689390ba
emil-devel <emil-devel@gmx.net>
2025-10-14 01:20:20 +0200

    seafoam theme

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,22 @@
 <script lang="ts">
-  import { Avatar } from '@skeletonlabs/skeleton-svelte/composed'
-  import { Github } from '@lucide/svelte'
+  import { Copyright, Github } from '@lucide/svelte'
+  import Header from './lib/Header.svelte'
 
   const outer = 'px-2 sm:py-4'
   const inner = 'w-full max-w-xl mx-auto  py-4 space-y-4'
 </script>
 
-<header class={outer}>
-  <div class="{inner} flex justify-between preset-filled-secondary-300-700 p-4">
-    <div class="mt-2 -mb-14 border-8 border-secondary-300-700 rounded-full w-fit h-fit">
-      <Avatar>
-        <Avatar.Image src="/ewd-logo@2x.svg" />
-      </Avatar>
-    </div>
-    <h1 class="text-2xl text-secondary-800-200">emil-devel</h1>
-  </div>
-</header>
+<Header {outer} {inner} />
 <main class="{outer} flex-auto">
   <article class={inner}>
-    <h2 class="text-2xl text-right">Hello world!</h2>
+    <h2 class="text-2xl text-right text-primary-300-700">Hello world!</h2>
   </article>
 </main>
 <footer class={outer}>
   <div class={inner}>
     <p class="text-center">
-      <span class="icon"><Github size={16} /></span>
-      <small>{new Date().getFullYear()} | emil-devel.github.io</small>
+      <span class="icon"><Copyright size={12} /></span>
+      <small>{new Date().getFullYear()} | <span class="icon"><Github size={16} /></span> emil-devel.github.io</small>
     </p>
   </div>
 </footer>
