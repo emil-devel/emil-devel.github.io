0590369b2f4c13bff96e97e204ce15978df321bd
emil-devel <emil-devel@gmx.net>
2025-10-13 03:56:37 +0200

    Header, head

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
+  import { Avatar } from '@skeletonlabs/skeleton-svelte/composed'
   import { Github } from '@lucide/svelte'
 
   const outer = 'px-2 sm:py-4'
-  const inner = 'w-full max-w-4xl mx-auto  py-4 space-y-4'
+  const inner = 'w-full max-w-xl mx-auto  py-4 space-y-4'
 </script>
 
 <header class={outer}>
-  <div class={inner}>
-    <h1 class="text-3xl">emil-devel</h1>
+  <div class="{inner} flex justify-between preset-filled-secondary-300-700 p-4">
+    <div class="mt-2 -mb-14 border-8 border-secondary-300-700 rounded-full w-fit h-fit">
+      <Avatar>
+        <Avatar.Image src="/ewd-logo@2x.svg" />
+      </Avatar>
+    </div>
+    <h1 class="text-2xl text-secondary-800-200">emil-devel</h1>
   </div>
 </header>
 <main class="{outer} flex-auto">
   <article class={inner}>
-    <h2 class="text-2xl">Hello world!</h2>
+    <h2 class="text-2xl text-right">Hello world!</h2>
   </article>
 </main>
 <footer class={outer}>
